ec4c6ce940eabe233ec7982285cf535e526743c8
psahni <prashant@vinsol.com>
2011-03-06 22:58:19 +0530

    Sunday commits

--- ruby-remote-db-postgres.rb
+++ ruby-remote-db-postgres.rb
@@ -1,34 +1,3 @@
-require 'rubygems'
-require 'active_record'
-require 'logger'
-require 'postgres'
-
-ActiveRecord::Base.logger = Logger.new(STDERR)
-ActiveRecord::Base.colorize_logging = false
-
-#LOGIC
-
-#1. Find users
-#2.
-#3. Find relationship
-
-conn = ActiveRecord::Base.establish_connection(
-	:adapter  => 'postgresql',
-	:database => 'osmdb',
-	:host => '24.229.182.156',
-	:port => '5432',
-	:username => 'osmuser',
-	:password => '$eed$'	
-)
-
-sql = "Select * from patients" 
-output = ActiveRecord::Base.connection.execute(sql)
-
-output.each{|o| 
-	p o
-	print "\n"
-}
-
 #http://efreedom.com/Question/1-2410373/Run-Sql-Script-Ruby
 #http://oldwiki.rubyonrails.org/rails/pages/PostgreSQL
 #http://ar.rubyonrails.org/
@@ -39,3 +8,45 @@ output.each{|o|
 #postgresql_autodoc -h 24.229.182.156 -d osmdb -u osmuser --password='$eed$'
 #dot -Tpng osmdb.dot > focusdirect-2.png
 #http://net.tutsplus.com/tutorials/ruby/setting-up-a-rails-server-and-deploying-with-capistrano-on-fedora-from-scratch/
+
+#  LOGIC
+
+#	 Find users
+#	 Select users with email id blank, Fetch email from responsible party table for those users  
+#	 Find relationship ()
+#	 Give one family id to the members
+
+#___________________________________________________________________________________________________________________________
+
+require 'rubygems'
+require 'active_record'
+require 'logger'
+require 'postgres'
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+ActiveRecord::Base.colorize_logging = false
+
+#___________________________________________________________________________________________________________________________
+
+conn = ActiveRecord::Base.establish_connection(
+	:adapter  => 'postgresql',
+	:database => 'osmdb',
+	:host => '24.229.182.156',
+	:port => '5432',
+	:username => 'osmuser',
+	:password => '$eed$'	
+)
+
+#___________________________________________________________________________________________________________________________
+
+sql = "Select * from patient_responsible_parties" 
+output = ActiveRecord::Base.connection.execute(sql)
+
+output.each{|o| 
+	p o
+	print "\n"
+}
+
+
+#___________________________________________________________________________________________________________________________
+
